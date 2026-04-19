ca9edcde1590203c26f1a792b1feffb3dedde2dd
Jaspreet Singh <js9685997@gmail.com>
2026-04-19 17:01:39 +0530

    Iframe everywhere

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,19 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
